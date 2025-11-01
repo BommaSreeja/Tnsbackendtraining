0e4805f8bcf3600dbd4aa95509b8317850bc87e2
BommaSreeja <sreejagoud024@gmail.com>
2025-11-01 11:50:42 +0530

    DEMO

---
+++ Tnsbackendtraining/src/demo/student.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class student {
+    int rollno;
+    String name;
+    String Branch;
+    static String collegename = "AVN";
+
+    void print() {
+        System.out.println("The details are: " + rollno + " " + name + " " + Branch + " " + collegename);
+    }
+}
+
---
+++ Tnsbackendtraining/src/demo/studentMain.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class studentMain {
+    public static void main(String[] args) {
+        student s1 = new student();
+        s1.rollno = 101;
+        s1.name = "Sreeja";
+        s1.Branch = "AIML";
+
+        s1.print();
+    }
+}
