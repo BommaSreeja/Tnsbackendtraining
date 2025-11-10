323423bdf30ccb5ded5d49ab4b7df03bf75f97ff
BommaSreeja <sreejagoud024@gmail.com>
2025-11-10 18:22:55 +0530

    Implementation of Inheritance

---
+++ Tnsbackendtraining/src/com/avn/tns/oopsdemo/Inheritance.java
@@ -0,0 +1,9 @@
+package com.avn.tns.oopsdemo;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        ElectricCar ec = new ElectricCar("Tesla", "xy", 75);
+        ec.startEngine();
+        ec.charge();
+    }
+}
---
+++ Tnsbackendtraining/src/com/avn/tns/oopsdemo/Vehicle.java
@@ -0,0 +1,19 @@
+package com.avn.tns.oopsdemo;
+
+//Demo for inheritance --> code Reuseability
+//parent,child-- superclass and subclass -- base class and derived class
+//Types of inheritance --> single,multilevel,hierarchy (not accepted multiple)
+public class Vehicle {
+  public String brand;
+  public String model;
+  
+  public Vehicle(String brand, String model) {
+      super();
+      this.brand = brand;
+      this.model = model;
+  }
+  
+  void startEngine() {
+      System.out.println("When engine is started the vehicle moves");
+  }
+}
